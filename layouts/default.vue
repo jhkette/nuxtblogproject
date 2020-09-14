3f78481bf7b4a88ddbc35b421085f31f0e5baf6a
khenryjoe <joseph.ketterer@gmail.com>
2020-09-14 14:45:13 +0100

    f

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
-    <Nuxt />
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false" />
+    <nuxt/>
   </div>
 </template>
 
+<script>
+import TheHeader from '@/components/Navigation/TheHeader'
+import TheSidenav from '@/components/Navigation/TheSidenav'
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
+}
+</script>
+
+
 <style>
 html {
-  font-family:
-    'Open Sans',
-    sans-serif
-}
-body {
-  margin: 0;
-  padding: 0;
+  font-family: 'Open Sans', sans-serif;
 }
 
+body {
+  margin: 0;
+}
 </style>
