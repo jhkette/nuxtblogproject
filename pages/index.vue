ac1c751d58e384fa2b1b5fdd178be5a739e932cc
khenryjoe <joseph.ketterer@gmail.com>
2020-09-14 13:30:24 +0100

    first

---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news</h1>
+    </section>
+    <section class="featured-posts">
+      <PostPreview 
+      id="1"
+      thumbnail = "https://i.guim.co.uk/img/media/e6d40cb644691606c6f9d9779fb6504d5b254c4a/0_333_5000_3000/master/5000.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=6ea7e1d5600e5c19a9d288489a43c072"
+      title = "Hello there"
+      previewText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium ea eaque quaerat repudiandae minus, neque, vitae reiciendis et veritatis laboriosam ipsam corrupti delectus alias perspiciatis quos quisquam, nobis tempora distinctio?"
+      />
+      <PostPreview 
+      id="2"
+      thumbnail = "https://i.guim.co.uk/img/media/e6d40cb644691606c6f9d9779fb6504d5b254c4a/0_333_5000_3000/master/5000.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=6ea7e1d5600e5c19a9d288489a43c072"
+      title = "Hello there"
+      previewText = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium ea eaque quaerat repudiandae minus, neque, vitae reiciendis et veritatis laboriosam ipsam corrupti delectus alias perspiciatis quos quisquam, nobis tempora distinctio?"
+      />
+     
+    </section>
+  </div>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components: {
+    PostPreview
+  }
+};
+</script>
+
+<style>.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-image: url('~assets/images/main-page-background.jpg');
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updated on XXX</div>
+        <div class="post-detail">Written by NAME</div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Delectus est incidunt animi
+        odio excepturi eos sapiente ipsam officia earum, laborum at ex? Officia
+        nesciunt nam accusantium sequi, reprehenderit atque quae.
+      </p>
+    </section>
+    <section class="post-feedback">
+      <p class="post-content">
+        Message to <a href="mailto:joseph22@gmail.com">joseph22@gmail.com</a>
+      </p>
+    </section>
+  </div>
+</template>
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+</style>
+
+<script>
+export default {};
+</script>
